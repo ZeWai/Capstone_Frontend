1b1ffc2f41df9464dbbc1e59fde44ec11a3db096
FONG MIO <miofong@live.hk>
2022-02-06 23:57:39 +0800

    update admin page

--- src/Page/Farmer/Content/Content.js
+++ src/Page/Farmer/Content/Content.js
@@ -4,22 +4,30 @@ import { useSelector, useDispatch } from "react-redux";
 import { GetClientNameThunk } from '../../../store/user/actions';
 
 export default function Content() {
-
+    const [location, setLocation] = useState(clientname);
     const clientname = useSelector((state) => state.userStore.clientNames);
+    const clientzone = useSelector((state) => state.userStore.clientZone);
     const dispatch = useDispatch();
-
     useEffect(() => {
         dispatch(GetClientNameThunk());
     }, []);
+
+    const getlocation = ((e) => {
+        console.log(e.currentTarget.value)
+        setLocation(e.currentTarget.value)
+    })
     return <div className='farmer_content'>
         <span>Farm Planner | floor plan</span>
-        <select name="location" className='dropdown'>
+        <select value={location} className='dropdown' onChange={(e) => (getlocation(e))} defaultValue="hi">
             {clientname && clientname.length > 0 ? clientname.map((client) => <option key={client.username} value={client.username}>{client.username}</option>) : <option>No Client</option>}
         </select>
         <br />
         <div className="slidebtn">
             <button>Overview</button>
-            {/* {zones && zones.length > 0
+            {clientzone && clientzone.length > 0 ? clientzone.map((zone) => <span>{zone.zone}</span>) : <span>No zone</span>}
+            <span>Location:</span> <span >{location}</span>
+            {/* <p>{setTest}</p> /}
+            {/ {zones && zones.length > 0
                 ? zones.map((zones, index) => (
                     <>
                         <button>{zones.name}</button>
