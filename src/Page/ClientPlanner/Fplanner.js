ed10ae3c34a8fb24203f09938114650a5c83ab94
martinho <kit.hmk@gmail.com>
2022-02-11 07:47:00 +0800

    mas

---
+++ src/Page/ClientPlanner/Fplanner.js
@@ -0,0 +1,12 @@
+
+import { Clientplanner } from './Clientplanner';
+import { DashNavbar } from '../Dashboard/Navbar';
+import './Clientplanner.css'
+export function Fplanner  ()  {
+    return (
+    <div>
+    <DashNavbar />
+    <Clientplanner />
+    </div>
+    )
+}
