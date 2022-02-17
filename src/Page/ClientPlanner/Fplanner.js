e0f4930743e95b4575608485e54f629d2accb635
fankilky <91205739+fankilky@users.noreply.github.com>
2022-02-17 20:01:30 +0800

    little change

--- src/Page/ClientPlanner/Fplanner.js
+++ src/Page/ClientPlanner/Fplanner.js
@@ -1,12 +1,11 @@
-
-import { Clientplanner } from './Clientplanner';
-import { DashNavbar } from '../Dashboard/Navbar';
-import './Clientplanner.css'
-export function Fplanner  ()  {
-    return (
+import { Clientplanner } from "./Clientplanner";
+import { DashNavbar } from "../Dashboard/Navbar";
+import "./Clientplanner.css";
+export function Fplanner() {
+  return (
     <div>
-    <DashNavbar Onview="scheduled"/>
-    <Clientplanner />
+      <DashNavbar Onview="scheduled" />
+      <Clientplanner />
     </div>
-    )
+  );
 }
